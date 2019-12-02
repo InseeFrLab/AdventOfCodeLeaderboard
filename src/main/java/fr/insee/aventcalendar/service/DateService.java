6a52035a7d0dc63b815189ec7b7d9f851b399e7f
Olivier Levitt <olivier.levitt@insee.fr>
2019-12-02 15:30:20 +0200

    Allow timezone override

--- src/main/java/fr/insee/aventcalendar/service/DateService.java
+++ src/main/java/fr/insee/aventcalendar/service/DateService.java
@@ -1,22 +1,22 @@
 package fr.insee.aventcalendar.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
 @Service
 public class DateService {
-    private SimpleDateFormat simpleDateFormat;
 
-    public DateService(@Value("${date.format}") String dateFormat) {
-        simpleDateFormat = new SimpleDateFormat(dateFormat);
-    }
+    @Autowired
+    private DateFormat dateFormat;
 
     public String formatDate(Long timestamp) {
         Date date = new Date(timestamp*1000);
-        return simpleDateFormat.format(date);
+        return dateFormat.format(date);
     }
 }
